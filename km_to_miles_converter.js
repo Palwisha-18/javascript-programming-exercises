ed2ce98a1a3563b1becce6bddc50b45d4ed16a1b
Palwisha-18 <palwisha.akhtar@arbisoft.com>
2023-01-13 11:07:43 -0700

    Add unit in output

--- km_to_miles_converter.js
+++ km_to_miles_converter.js
@@ -5,4 +5,4 @@ const converToMiles = function(valueInkm) {
 const prompt = require("prompt-sync")({ sigint: true });
 const number = parseInt(prompt('Enter values in kilometers: '));
 
-console.log(converToMiles(number));
+console.log(`${converToMiles(number)} miles`);
