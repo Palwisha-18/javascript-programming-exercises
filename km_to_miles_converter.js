a1fb7012cbcbf0c33bb9c2cee3d67018fcc6bec3
Palwisha-18 <palwisha.akhtar@arbisoft.com>
2023-01-13 11:04:34 -0700

    Use function expression for km to mile converter

---
+++ km_to_miles_converter.js
@@ -0,0 +1,8 @@
+const converToMiles = function(valueInkm) {
+    return valueInkm * 0.621371
+}
+
+const prompt = require("prompt-sync")({ sigint: true });
+const number = parseInt(prompt('Enter values in kilometers: '));
+
+console.log(converToMiles(number));
